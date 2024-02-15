4db56f64b6ab6eb74d7eab36e890e4f54bbd3e2b
nikilok <nikilok@gmail.com>
2024-02-15 15:16:33 +0000

    Add server side validations for create page

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -8,17 +8,48 @@ const prisma = new PrismaClient();
 
 const FormSchema = z.object({
   id: z.string(),
-  status: z.enum(['pending', 'paid']),
-  amount: z.coerce.number(),
+  status: z.enum(['pending', 'paid'], {
+    invalid_type_error: 'Please select an invoice status.',
+  }),
+  amount: z.coerce
+    .number()
+    .gt(0, { message: 'Please enter an amount greater than $0.' })
+    .lt(100000, { message: 'Please enter an amount less than $100,000' }),
   date: z.string(),
-  customerId: z.string(),
+  customerId: z.string({
+    invalid_type_error: 'Please select a valid customer',
+  }),
 });
 
 const CreateInvoice = FormSchema.omit({ id: true, date: true });
 
-export async function createInvoice(formData: FormData) {
-  const rawFormData = Object.fromEntries(formData.entries());
-  const { status, customerId, amount } = CreateInvoice.parse(rawFormData);
+export type State = {
+  errors?: {
+    customerId?: string[];
+    amount?: string[];
+    status?: string[];
+  };
+  message?: string | null;
+};
+
+export async function createInvoice(prevState: State, formData: FormData) {
+  //   const rawFormData = Object.fromEntries(formData.entries());
+
+  const validatedFields = CreateInvoice.safeParse({
+    customerId: formData.get('customerId'),
+    amount: formData.get('amount'),
+    status: formData.get('status'),
+  });
+
+  // If form validation fails, return errors early. Otherwise, continue.
+  if (!validatedFields.success) {
+    return {
+      errors: validatedFields.error.flatten().fieldErrors,
+      message: 'Missing Fields. Failed to Create Invoice.',
+    };
+  }
+
+  const { status, customerId, amount } = validatedFields.data;
   const amountInCents = amount * 100;
   const date = new Date().toISOString();
 
--- app/ui/invoices/clientButtons.tsx
+++ app/ui/invoices/clientButtons.tsx
@@ -11,3 +11,13 @@ export function EditSubmitButton() {
     </Button>
   );
 }
+
+export function CreateInvoiceButton() {
+  const { pending } = useFormStatus();
+
+  return (
+    <Button type="submit" disabled={pending}>
+      {pending ? 'Creating...' : 'Create Invoice'}
+    </Button>
+  );
+}
